797e4d88e33f105f1128bb758d7fc7c6aff25b44
Christiaan <christiaandpreez@gmail.com>
2020-11-23 19:27:51 +0200

    finished login and register (no jwt yet)

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../models/User");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt");
 
 //Gets back all users
 router.get("/", async (req, res) => {
@@ -15,10 +15,10 @@ router.get("/", async (req, res) => {
 	}
 });
 //Submits user
-router.post("/", async (req, res) => {
+router.post("/register", async (req, res) => {
 	const user = new User({
 		username: req.body.username,
-		password: req.body.password
+		password: req.body.password,
 	});
 
 	console.log(req.body);
@@ -30,11 +30,18 @@ router.post("/", async (req, res) => {
 	}
 });
 
-//Specific user
-router.get("/:tuserName", async (req, res) => {
+//Log in
+router.post("/login", async (req, res) => {
 	try {
-		const user = await User.findById(req.params.tuserName);
-		res.json(user);
+		//Test Userename
+		const username = await User.findOne({ username: req.body.username });
+		if (!username) return res.status(400).send("Username Invalid");
+
+		//Test Password
+		const password = await User.findOne({ password: req.body.password });
+		if (!password) return res.send("Password Invalid");
+
+		res.send("validated");
 	} catch (err) {
 		res.json({ message: err });
 	}
@@ -56,7 +63,7 @@ router.patch("/:userId", async (req, res) => {
 		const updateUser = await User.updateOne(
 			{ _id: req.params.userId },
 			{ $set: { username: req.body.username } }
-        );
+		);
 		res.json(updateUser);
 	} catch (err) {
 		res.json({ message: err });
